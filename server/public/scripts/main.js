f6c5e65ff7e2b6ed824bba9a815e2def19e576fb
Luke <shiffer@usc.edu>
2018-06-29 14:50:56 -0700

    Styles and Output Styles added for the html page. Log data is output

--- server/public/scripts/main.js
+++ server/public/scripts/main.js
@@ -9,6 +9,15 @@ async function init() {
 
 function socketListener() {
 	socket.on('newLog', function(data) {
-		console.log(data);
+		let output = `<div class="label uuid">` + data.uuid + `</div>
+					<div class="label dateTime">` + data.dateTime + `</div>
+					<div class="label method">` + data.method + `</div>
+					<div class="label url">` + data.url + `</div>
+					<div class="label duration">` + data.duration + `</div>
+					<div class="label strings">` + data.stats.strings + `</div>
+					<div class="label memoryUsage">` + data.stats.memoryUsage + `</div>
+					<br style="clear:both"/>`;
+		let container = document.getElementById('events');
+		container.innerHTML = output + container.innerHTML;
 	});
 }
